dc51d0aaaa70bbb4c3f604d3670434de57ff32c0
Dinuka-Sandaruwan-Warnasooriya <dinukaswarnasooriya@gmail.com>
2022-04-24 22:56:33 +0530

    Invoice Edit Class Added

--- BillingInvoice/src/main/java/net/invoice/we/EditInvoice.java
+++ BillingInvoice/src/main/java/net/invoice/we/EditInvoice.java
@@ -2,8 +2,44 @@ package net.invoice.we;
 
 public class EditInvoice {
 
-	public EditInvoice() {
+
 		// TODO Auto-generated constructor stub
-	}
+		String Invoice_ID; 
+		String Invoice_status;
+		float Normal_unitPrice;
+		float Peak_unitPrice;
+		String Due_date;
+		String Issued_date;  
+		float Incoice_amount;
+		@Override
+		public int hashCode() {
+			// TODO Auto-generated method stub
+			return super.hashCode();
+		}
+		@Override
+		public boolean equals(Object obj) {
+			// TODO Auto-generated method stub
+			return super.equals(obj);
+		}
+		@Override
+		protected Object clone() throws CloneNotSupportedException {
+			// TODO Auto-generated method stub
+			return super.clone();
+		}
+		@Override
+		public String toString() {
+			// TODO Auto-generated method stub
+			return super.toString();
+		}
+		@Override
+		protected void finalize() throws Throwable {
+			// TODO Auto-generated method stub
+			super.finalize();
+		}
+		public EditInvoice() {
+			super();
+			// TODO Auto-generated constructor stub
+		}
+	
 
 }
